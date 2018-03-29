cef3b936567906d9d6a38760085903a5cc1c15b5
srauscher <rauscher-ff3l@t-online.de>
2018-03-29 15:40:41 +0200

    Update files

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
+if (!String.prototype.includes) {
+  String.prototype.includes = function (){
+    return String.prototype.indexOf.apply(this, arguments) !== -1;
+  };
+}
+
 require.config({
   baseUrl: "lib",
   paths: {
